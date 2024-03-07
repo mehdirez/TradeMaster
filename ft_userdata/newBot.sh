9efa4a6b15adb3fb3635eef42f133f4152f80375
mehdi <mehdirez2013@gmail.com>
2024-03-08 00:30:45 +1100

    epochs update

--- ft_userdata/newBot.sh
+++ ft_userdata/newBot.sh
@@ -21,17 +21,17 @@ sudo docker-compose pull
 # remove past trade data
 rm user_data/tradesv3.sqlite
 # download data for backtesting
-sudo docker-compose run --rm freqtrade download-data --exchange binance -t 15m
+sudo docker-compose run --rm freqtrade download-data --exchange binance -t 15m --timerange=20230101-
 
 # optinal backtesting command
 # docker-compose run freqtrade backtesting --datadir user_data/data/binance --export trades  --stake-amount 1000 -s SwingHighToSky -i 15m --timerange=20210728-
 
 # Run Optimization
-sudo docker-compose run --rm freqtrade hyperopt --enable-protections --strategy SwingHighToSky --hyperopt-loss SharpeHyperOptLoss -i 15m -e 5000
+# sudo docker-compose run --rm freqtrade hyperopt --enable-protections --strategy SwingHighToSky --hyperopt-loss SharpeHyperOptLoss -i 15m -e 5000 --timerange=20230101-
 
 # git add .
 # git commit -m "Hyperopt new updatte"
 # git pull origin main
 # git push origin branch-name #we need ssh key for pushing
-# sudo docker-compose up -d
-# sudo docker-compose logs -f
+sudo docker-compose up -d
+sudo docker-compose logs -f
